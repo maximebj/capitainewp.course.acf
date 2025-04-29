ee804b7f4e6212aa847bc63703a71a9ab6f1b613
Maxime BJ <maxime@dysign.fr>
2025-04-29 09:00:14 +0200

    improved options pages declaration

--- functions.php
+++ functions.php
@@ -159,26 +159,28 @@ add_action('init', 'capitaine_register_post_types');
 # Pages d'options
 function capitaine_register_acf_options_pages()
 {
-
-  if (function_exists('acf_add_options_page')) {
-
-    acf_add_options_page(array(
-      'page_title'   => 'Options du thème',
-      'menu_title'  => 'Options',
-      'menu_slug'   => 'theme-general-settings',
-      'capability'  => 'edit_posts',
-      'redirect'    => false,
-      'position'    => 2
-    ));
-
-    acf_add_options_sub_page(array(
-      'page_title'   => 'Couleurs du thème',
-      'menu_title'  => 'Couleurs',
-      'parent_slug'  => 'theme-general-settings',
-    ));
+  if (!function_exists('acf_add_options_page')) {
+    return;
   }
+
+  # Page d'options principale
+  acf_add_options_page(array(
+    'page_title'   => 'Options du thème',
+    'menu_title'  => 'Options',
+    'menu_slug'   => 'theme-general-settings',
+    'capability'  => 'edit_posts',
+    'redirect'    => false,
+    'position'    => 2
+  ));
+
+  # Sous-page pour les couleurs
+  acf_add_options_sub_page(array(
+    'page_title'   => 'Couleurs du thème',
+    'menu_title'  => 'Couleurs',
+    'parent_slug'  => 'theme-general-settings',
+  ));
 }
-add_action('init', 'capitaine_register_acf_options_pages');
+add_action('acf/init', 'capitaine_register_acf_options_pages');
 
 
 
